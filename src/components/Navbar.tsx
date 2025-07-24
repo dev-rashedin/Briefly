88ec241ae5f37e8f87683d71278eea61d35de287
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-24 10:23:11 +0600

    dialoge addd and login button separated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,19 @@
-import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import ModeToggle from "./mode-toggle"
+import LoginButton from './login-button';
 
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between">
-      <div className="logo text-xl md:text-4xl">
-       <Link href="/">Briefly</Link>
+    <div className='flex justify-between'>
+      <div className='logo text-xl md:text-4xl'>
+        <Link href='/'>Briefly</Link>
       </div>
-      <div className="flex gap-4">
-        <Button size='lg'>Sign in</Button>
-      <ModeToggle/>
+      <div className='flex gap-4'>
+        <LoginButton />
+        <ModeToggle />
       </div>
     </div>
-  )
+  );
 }
 export default Navbar
---
+++ src/components/login-button.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+import { Button } from './ui/button';
+
+const LoginButton = (): JSX.Element => {
+  return (
+    <div>
+      <Button size='lg'>Sign in</Button>
+    </div>
+  );
+};
+export default LoginButton;
